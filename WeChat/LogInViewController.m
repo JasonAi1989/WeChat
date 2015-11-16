07db3e455613ed40a234736a94aed37768dec5a7
jason <aizhaoyu@163.com>
2015-11-17 03:04:24 +0800

    I'm gonna die

--- WeChat/LogInViewController.m
+++ WeChat/LogInViewController.m
@@ -8,7 +8,8 @@
 
 #import "LogInViewController.h"
 
-@interface LogInViewController ()
+@interface LogInViewController ()<UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -39,5 +40,37 @@
 -(void)UILayout{
     self.navigationController.navigationBar.hidden = false;
     
+    self.tableView.dataSource = self;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString* identify = @"LogIn";
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:identify];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identify];
+        
+        UILabel *countryLabel = [[UILabel alloc]initWithFrame:CGRectMake(15, 13.5, 125.5, 21.5)];
+        countryLabel.text = @"Country/Region";
+        
+//        UILabel *chinaLabel = [[UILabel alloc]initWithFrame:CGRectMake(338.5, 13.5, 46.5, 21.5)];
+        UILabel *chinaLabel = [[UILabel alloc]init];
+//        CGRect rect = chinaLabel.frame;
+//        rect.size = CGSizeMake(46.5, 21.5);
+//        chinaLabel.frame = rect;
+        chinaLabel.text = @"China";
+        chinaLabel.textColor = [[UIColor alloc]initWithRed:0 green:216/255.0 blue:0 alpha:1];
+        
+        [cell.contentView addSubview:countryLabel];
+        [cell.contentView addSubview:chinaLabel];
+        
+        [cell.contentView addConstraint:[NSLayoutConstraint constraintWithItem:chinaLabel attribute:NSLayoutAttributeRightMargin relatedBy:NSLayoutRelationEqual toItem:cell.contentView attribute:NSLayoutAttributeRightMargin multiplier:1.0 constant:7]];
+//        [cell.contentView addConstraint:[NSLayoutConstraint constraintWithItem:chinaLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:cell.contentView attribute:NSLayoutAttributeTop multiplier:1.0 constant:20]];
+    }
+    
+    return cell;
 }
 @end
